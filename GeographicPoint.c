78dadbaef0671736277850901e7ad949f5f098ce
Johana Barrera <ja.barrera10@uniandes.edu.co>
2017-07-27 08:52:06 -0700

    Realice una funcion para calcular el radio conociendo el punto de comienzo y el final, tambien una funcion que retorna numeros aleatorios en la matriz y las condiciones de frontera

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -2,70 +2,72 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<time.h>
 
+// Variables universales:
 int fil;
 int col;
 
+// Orden en que se declararon las funciones que se encuentran definidas despues del main.
+
 int **Matriz(void);
 
-// FORMA 1 DE LEER EL ARCHIVO.
-/*
-int main()
-{
-	FILE *archivo;
- 	int len=372000;
- 	//int val;
- 	char *caracteres=malloc(len*sizeof(char));
- 	
- 	archivo = fopen("map_data.txt","r");
- 	
- 	if (archivo == NULL)
- 		exit(1);
- 	
- 	//printf("\nEl contenido del archivo de prueba es \n\n");
- 	while (feof(archivo) == 0)
- 	{
- 		fgets(caracteres,len,archivo);
- 		//val=atoi(caracteres);
- 		printf("%d",val);
- 	}
-        system("PAUSE");
+void rand(int *ind);
 
-        fclose(archivo);
-	return 0;
-}
-*/
+int index_periodic(int i, int j, int count);
 
-// SEGUNDA FORMA DE LEER ARCHIVO.
+double Calculo_radio(int x0, int x, int y0, int y);
+
+void Libera(int **matrix);
+
+//void Calculo_radio(int pt_alea );
+
 
 int main()
 {
 
 	fil=500;
 	col=744;
-	int **p=Matriz();
+	int **matriz=Matriz();
 	int i;
 	int j;
-
+/*
 for ( i = 0; i < fil; ++i)
 {
 	for (j = 0; j <col ; ++j)
 	{
 		
-			printf("%d\t", p[i][j]);
+			printf("%d\t", matriz[i][j]);
 		
 		
 	}
 	printf("\n");
 }
+*/
+	/*
+	int a=0;
+	for ( i = 0; i < fil; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			while(matriz[i][j+a]==0 && matriz[i][j-a]==0 && matriz[i+a][j]==0 && matriz[i-a][j]==0)
+			{
+				
+					a=a+1;
 
+			}
+		}
+	}
+	//printf("%e\n", Calculo_radio(1, 2, 1, 2) );
+*/
+	printf("%d", index_periodic( 499, 300, 1));
 
-
-
+	Libera(matriz);
 	return 0;
 }
 
-int **Matriz()
+// La siguiente funcion retorna los datos map_data.txt guardados en una matriz.
+int **Matriz(void)
 {
 	FILE *file;
     file=fopen("map_data.txt", "r");
@@ -103,19 +105,9 @@ int **Matriz()
       
       val= atoi(separar);
      
-      
-      //printf("el trozo es :%s\n", separar );
-      
-      //printf("en doble es: %d\n", val);
-      //if (i<fil && j<col)
-      //{
-      	matriz[i][j]=val;
-      //}
-      
-      //printf("%d", matriz[i][j] );
-      
 
-      
+      	matriz[i][j]=val;
+
       
       separar=strtok(NULL, delim);
       
@@ -130,6 +122,60 @@ int **Matriz()
 	}
 
 return matriz;
+}
+
+
+
+void rand(int *ind)
+{
+	int  a, b ;
+	srand(time(NULL));
+	a=rand()%(500);
+	srand(time(NULL));
+	b=rand()%(744);
+	while(matriz[a][b]!=0){
+		a=rand()%(500);
+		b=rand()%(744);
+	}
+	//printf("%d , %d\n", i,j);
+	index[0]=a;
+	index[1]=b;
+}
+
+double Calculo_radio(int x0, int x, int y0, int y)
+{
+	int r;
+	r=pow((x0-x),2.0)+pow((y0-y),2.0);
+
+	return pow(r,(1.0/2.0));
+}
+
+int index_periodic(int i, int j, int count)
+{
+	if ( (i+count)>(fil-count) )
+	{
+		int a= (i+count)%500;
+	
+		return a;
+	}
+	if ( (j+count)>(col-count) )
+	{
+		int b= (j+count)%744;
+
+		return b;
+	}
+	if ((i-count)<0 )
+	{
+		int c=(i-count)+fil;
+		
+		return c;
+	}
+	if ((j-count)<0)
+	{
+		int d=(j-count)+col;
+
+		return d;
+	}
 }
 
 void Libera(int **matrix)
